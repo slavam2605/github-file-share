ea76a4e44f2b504d95596c31167d5374857c01fd
Moklev Vyacheslav <slavam2605@bk.ru>
2018-03-19 09:59:17 +0000

    Scripts for OpenCV build

---
+++ install_dependencies.sh
@@ -0,0 +1,4 @@
+=!/bin/sh
+sudo spt-get install build-essential
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libtiff-dev
